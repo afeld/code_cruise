8dd1884ec7aae4e4adb87698d93cc9baf55cacd0
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-01 16:46:52 -0400

    display children on topic pages

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,7 @@
 class Topic < ActiveRecord::Base
   has_many :parent_relationships, class_name: 'Relationship', foreign_key: :child_id
   has_many :parents, through: :parent_relationships
+
+  has_many :child_relationships, class_name: 'Relationship', foreign_key: :parent_id
+  has_many :children, through: :child_relationships
 end
