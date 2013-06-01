dd34ae7badde2d6254ba0563a08de062c2c10eed
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-01 16:19:25 -0400

    use bower

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery
+// use the Bower version
+//= require jquery/jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
