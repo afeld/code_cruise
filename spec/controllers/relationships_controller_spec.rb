1f08329f42b01284261def5c0eeb7f1eb19daf0f
Aidan Feldman <aidan.feldman@gmail.com>
2013-06-01 23:01:25 -0400

    fix tests (and remove the useless ones)

--- spec/controllers/relationships_controller_spec.rb
+++ spec/controllers/relationships_controller_spec.rb
@@ -23,7 +23,7 @@ describe RelationshipsController do
   # This should return the minimal set of attributes required to create a valid
   # Relationship. As you add validations to Relationship, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { { "parent_id" => "1" } }
+  let(:valid_attributes) { { parent_id: 1, child_id: 2 } }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in
