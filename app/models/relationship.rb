3342f0dcd652eea71e8049f1eb1d27d8d2c4b30e
Aidan Feldman <aidan.feldman@gmail.com>
2013-07-19 02:00:51 -0400

    dont allow relationships to be self-referential

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,4 +4,7 @@ class Relationship < ActiveRecord::Base
 
   validates :parent_id, presence: true
   validates :child_id, presence: true, uniqueness: { scope: :parent_id }
+  validate do |rel|
+    errors.add(:child_id, "must not be the same as the parent") if rel.parent_id == rel.child_id
+  end
 end
