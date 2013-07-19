06fff285557f320485fccb7181bda2e5070f4eff
Aidan Feldman <aidan.feldman@gmail.com>
2013-07-19 00:08:45 -0400

    nicer formatting of alternate names on homepage

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,7 @@
 module HomeHelper
+  def pretty_alternate_titles(topic)
+    list = topic.alternate_titles.map{|alt| "\"#{alt.title}\"" }
+    output = list[0...-1].join(', ').presence
+    [output, list.last].compact.join(' or ')
+  end
 end
--- spec/helpers/home_helper_spec.rb
+++ spec/helpers/home_helper_spec.rb
@@ -11,5 +11,39 @@ require 'spec_helper'
 #   end
 # end
 describe HomeHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "pretty_alternate_titles()" do
+    it "should output an empty string for no alternates" do
+      topic = Topic.new
+      helper.pretty_alternate_titles(topic).should eq('')
+    end
+
+    it "should output a single alternate as-is" do
+      topic = Topic.create!(title: 'foo')
+      topic.alternate_titles.create!(title: 'bar')
+      helper.pretty_alternate_titles(topic).should eq('"bar"')
+    end
+
+    it "should output a single alternate as-is" do
+      topic = Topic.create!(title: 'foo')
+      topic.alternate_titles.create!(title: 'bar')
+      helper.pretty_alternate_titles(topic).should eq('"bar"')
+    end
+
+    it "should output two alternates with an 'or'" do
+      topic = Topic.create!(title: 'foo')
+      topic.alternate_titles.create!(title: 'bar')
+      topic.alternate_titles.create!(title: 'baz')
+
+      helper.pretty_alternate_titles(topic).should eq('"bar" or "baz"')
+    end
+
+    it "should multiple alternates with commas, then an 'or'" do
+      topic = Topic.create!(title: 'foo')
+      topic.alternate_titles.create!(title: 'bar')
+      topic.alternate_titles.create!(title: 'baz')
+      topic.alternate_titles.create!(title: 'blip')
+
+      helper.pretty_alternate_titles(topic).should eq('"bar", "baz" or "blip"')
+    end
+  end
 end
